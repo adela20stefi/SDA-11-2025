7b39882fc50c437a6573f0a49f11145baf4b52f4
adela20stefi <73963242+adela20stefi@users.noreply.github.com>
2025-05-31 10:00:26 +0000

    pow_of_n

---
+++ 01-Programing-language/_Power_of_two/Solution.java
@@ -0,0 +1,20 @@
+public class Solution {
+    /**
+     * Returns true if n is a power of two, false otherwise.
+     * @param n the integer to check
+     * @return boolean indicating if n is a power of two
+     */
+    public boolean isPowerOfTwo(int n) {
+        // A power of two has exactly one bit set in its binary representation
+        // n > 0 ensures we don't consider negative numbers or zero
+        return n > 0 && (n & (n - 1)) == 0;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] testCases = {1, 2, 3, 4, 16, 18, 0, -2};
+        for (int n : testCases) {
+            System.out.println("isPowerOfTwo(" + n + ") = " + sol.isPowerOfTwo(n));
+        }
+    }
+}
